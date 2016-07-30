eaa214a5ea0910b51c4258b550a314165953d6a8
Tom Stapersma <info@caelaris.com>
2016-07-30 23:21:25 +0200

    Started path validation

--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -3,7 +3,13 @@
 namespace Caelaris\Arcade\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\Input;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
 
 /**
  * Class InitCommand
@@ -20,6 +26,56 @@ class InitCommand extends Command
     {
         $this->setName('init')
             ->setDescription('Initialize a new Symfony Console application')
-            ->addOption(self::OPTION_PATH, self::SHORT_OPTION_PATH, InputArgument::REQUIRED, 'Path in which to initialize the new Console application');
+            ->addOption(self::OPTION_PATH, self::SHORT_OPTION_PATH, InputOption::VALUE_REQUIRED, 'Path in which to initialize the new Console application');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+
+    /**
+     * Interact with the user
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<info>This command will help you to quickly setup a new console application</info>');
+
+        $path = $this->validatePath($input, $output);
+//        /** @var QuestionHelper $helper */
+//        $helper = $this->getHelper('question');
+//        $path = $input->getOption(self::OPTION_PATH);
+//        if (null !== $path) {
+//            $path = realpath($path);
+//            if (!$helper->ask($input, $output, $question)) {
+//                $newPathQuestion = new Question('Where would you like your new application to be created: ');
+//                $path = $helper->ask($input, $output, $newPathQuestion);
+//                echo realpath($path);
+//            }
+//        }
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @param null $confirmationQuestion
+     */
+    public function validatePath(InputInterface $input, OutputInterface $output, $confirmationQuestion = null)
+    {
+        $path = $input->getOption(self::OPTION_PATH);
+
+        if (null === $path) {
+            return realpath('.');
+        }
+
+        if (null === $confirmationQuestion) {
+            $confirmationQuestion = new ConfirmationQuestion('Your new application will be created at <comment>' . $path . '</comment>.' . PHP_EOL . 'Do you want to change the installation path [y/N]: ', false);
+        }
+        /** @var QuestionHelper $helper */
+        $helper = $this->getHelper('question');
+        $helper->ask($input, $output, $confirmationQuestion);
     }
 }
--- tests/Command/InitCommandTest.php
+++ tests/Command/InitCommandTest.php
@@ -7,6 +7,11 @@ use Caelaris\Arcade\Command\InitCommand;
 class InitCommandTest extends PHPUnit_Framework_TestCase
 {
     const CLASS_NAME_SYMFONY_INPUT_OPTION = '\Symfony\Component\Console\Input\InputOption';
+    const CLASS_NAME_SYMFONY_INPUT_INTERFACE = '\Symfony\Component\Console\Input\InputInterface';
+    const CLASS_NAME_SYMFONY_OUTPUT_INTERFACE = '\Symfony\Component\Console\Output\OutputInterface';
+    const CLASS_NAME_SYMFONY_HELPER_SET = '\Symfony\Component\Console\Helper\HelperSet';
+    const CLASS_NAME_SYMFONY_QUESTION_HELPER = '\Symfony\Component\Console\Helper\QuestionHelper';
+    const CLASS_NAME_CONFIRMATION_QUESTION = '\Symfony\Component\Console\Question\ConfirmationQuestion';
     /**
      * @var InitCommand
      */
@@ -44,5 +49,35 @@ class InitCommandTest extends PHPUnit_Framework_TestCase
         $pathOption = $this->command->getDefinition()->getOption(InitCommand::OPTION_PATH);
         $expected = self::CLASS_NAME_SYMFONY_INPUT_OPTION;
         $this->assertInstanceOf($expected, $pathOption);
+    }
+
+    /**
+     *
+     */
+    public function testValidatePathAsksForConfirmation()
+    {
+        $questionHelperMock = $this->getMock(self::CLASS_NAME_SYMFONY_QUESTION_HELPER);
+        $helperSetMock = $this->getMock(self::CLASS_NAME_SYMFONY_HELPER_SET);
+        $helperSetMock->expects($this->once())->method('get')->with('question')->willReturn($questionHelperMock);
+        /** @noinspection PhpParamsInspection */
+        $this->command->setHelperSet($helperSetMock);
+        $inputMock = $this->getMock(self::CLASS_NAME_SYMFONY_INPUT_INTERFACE);
+        $inputMock->expects($this->once())->method('getOption')->with(InitCommand::OPTION_PATH)->willReturn('\tmp\test');
+        $outputMock = $this->getMock(self::CLASS_NAME_SYMFONY_OUTPUT_INTERFACE);
+
+        $questionMock = $this->getMock(self::CLASS_NAME_CONFIRMATION_QUESTION, [], [], '', false);
+        $questionHelperMock->expects($this->once())->method('ask')->with($inputMock, $outputMock, $questionMock);
+
+        /** @noinspection PhpParamsInspection */
+        $this->command->validatePath($inputMock, $outputMock, $questionMock);
+    }
+
+    public function testValidatePathAsksNoQuestionIfPathOptionIsNotSet()
+    {
+        $inputMock = $this->getMock(self::CLASS_NAME_SYMFONY_INPUT_INTERFACE);
+        $inputMock->expects($this->once())->method('getOption')->with(InitCommand::OPTION_PATH)->willReturn(null);
+        $outputMock = $this->getMock(self::CLASS_NAME_SYMFONY_OUTPUT_INTERFACE);
+
+        $this->command->validatePath($inputMock, $outputMock);
     }
 }
